70a70b8b531cfcfbd8330fbc3113d76ce169d9c8
nncuong2506 <nhatcuong250601@gmail.com>
2025-10-25 19:55:28 +0700

    fix: add gunicorn for Render

--- app.py
+++ app.py
@@ -22,5 +22,9 @@ def predict():
 
     return jsonify({"answer": response})
 
+import os
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0',port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
